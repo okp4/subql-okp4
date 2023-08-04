4ea24c8ed4ee97c410f994ccfe5bd66b1a20b130
ErikssonJoakim <j.eriksson@okp4.com>
2023-08-04 15:17:22 +0200

    feat(handler): add pins to store object handler

--- src/mappings/handleStoreObject.ts
+++ src/mappings/handleStoreObject.ts
@@ -3,15 +3,36 @@ import type { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
 import { ObjectariumObject } from "../types";
 import { getObjectariumObjectId } from "../utils/utils";
 
+type StoreObject = {
+    pin: boolean;
+};
+
+type Msg = {
+    store_object: StoreObject;
+};
+
+type ObjectariumMsgExecuteContract = Omit<MsgExecuteContract, "msg"> & {
+    msg: Msg;
+};
+
 export const handleStoreObject = async (
-    msg: CosmosMessage<MsgExecuteContract>
+    msg: CosmosMessage<ObjectariumMsgExecuteContract>
 ): Promise<void> => {
     const objectId = getObjectariumObjectId(msg.tx.tx.events);
 
-    objectId &&
-        (await ObjectariumObject.create({
-            id: objectId,
-            sender: msg.msg.decodedMsg.sender,
-            contract: msg.msg.decodedMsg.contract,
-        }).save());
+    if (objectId) {
+        msg.msg.decodedMsg.msg.store_object.pin
+            ? await ObjectariumObject.create({
+                  id: objectId,
+                  sender: msg.msg.decodedMsg.sender,
+                  contract: msg.msg.decodedMsg.contract,
+                  pins: [msg.msg.decodedMsg.sender],
+              }).save()
+            : await ObjectariumObject.create({
+                  id: objectId,
+                  sender: msg.msg.decodedMsg.sender,
+                  contract: msg.msg.decodedMsg.contract,
+                  pins: [],
+              }).save();
+    }
 };
